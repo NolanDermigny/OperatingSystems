e494409970b4ea69fc47750b0c3debb2b68af242
Nolan Dermigny <nd4331@sloop.cs.ship.edu>
2025-02-19 23:02:45 -0500

    basic shell structure fro example + updated makefile

--- main.c
+++ main.c
@@ -6,43 +6,54 @@
 #include <stdbool.h>
 #include "wish.h"
 
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
+
 int main(int argc, char *argv[]) {
+	char command[MAX_COMMAND_LENGTH];
+	char *args[MAX_ARGUMENTS];
+	
+	while(1) {
+		printf("wish> ");
+		fflush(stdout);
 
-    int line = 1;
-    char buff[100];    //line buffer
-    int limit = 100;     //buffer limit
-    bool batch = 0;     //boolean for if in batch mode
+		//read command from user
+		if(fgets(command, sizeof(command), stdin) == NULL) {
+			printf("\n");
+			break;
+		}
 
-    if(argc == 2) {
+		//remove trailing '\n'
+		command[strcspn(command, "\n")] = '\0';
+		
+		//tokenize command into arguments
+		char *token = strtok(command, " ");
+		int arg_count = 0;
+		while(token != NULL && arg_count < MAX_ARGUMENTS - 1) {
+			args[arg_count++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[arg_count] = NULL;
 
-        //batch mode
-        FILE *file = fopen(argv[1], "r");
+		//fork() a child process
+		pid_t pid = fork();
 
-        //loops through the batch file printing out each line
-        while(feof(file) == 0) {
-            fgets(buff, limit, file);
-            if(feof(file)) {
-                break;
-            }
-            printf("line %d: %s\n", line, buff);
-            line++;
-        }
-    } else if(argc == 1) {
-        //interatcive mode
-        buff[0] = ' ';
-        //continuously prompts user and echos the input
-        while(strcmp(buff, "exit") != 0) {
-            printf("wish> ");
-            fgets(buff, limit, stdin);
-            printf("line %d: %s\n", line, buff);
-            line++;
-        }
+		if(pid < 0) {
+			perror("fork failed");
+			exit(EXIT_FAILURE);
+		} else if (pid == 0) {
+			//child process executes the command
+			execvp(args[0], args);
+			//this should not return
+			perror("execvp failed");
+			exit(EXIT_FAILURE);
+		} else {
+			//parent process waits for the child to complete
+			int status;
+			waitpid(pid, &status, 0);
+		}
+	}
 
-    } else {
-        //error
-        printf("error: too many arguments\n");
-        exit(0);
-    }
-
+	return 0;
 }
 
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 wish: main.o wish.o
 	gcc -o wish main.o wish.o
 
-main.o: wish.c
+main.o: main.c
 	gcc -c main.c
 
 wish.o: wish.c
